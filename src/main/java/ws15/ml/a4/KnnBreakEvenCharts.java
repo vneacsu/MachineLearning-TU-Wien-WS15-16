cf26996de5c33ce79c59594528c14ed297123321
stefanottinger <stefan.ottinger@student.tuwien.ac.at>
2016-01-22 16:29:07 +0100

    added plotting of line chart of total time per strategy and dataset (to be changed to a scatter plot, if possible)

--- src/main/java/ws15/ml/a4/KnnBreakEvenCharts.java
+++ src/main/java/ws15/ml/a4/KnnBreakEvenCharts.java
@@ -16,50 +16,103 @@ import weka.experiment.ResultProducer;
 
 import java.awt.*;
 import java.io.FileOutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.function.Consumer;
 
 import static org.krysalis.jcharts.properties.StockChartProperties.DEFAULT_STROKE;
+import static org.krysalis.jcharts.properties.PointChartProperties.SHAPE_SQUARE;
+import static org.krysalis.jcharts.properties.PointChartProperties.SHAPE_TRIANGLE;
+import static org.krysalis.jcharts.properties.PointChartProperties.SHAPE_CIRCLE;
+import static org.krysalis.jcharts.properties.PointChartProperties.SHAPE_DIAMOND;
 
 public class KnnBreakEvenCharts implements ResultProducer, Consumer<List<KnnEvaluation>> {
+
+    public static final Shape[] STANDARD_SHAPES = new Shape[]{SHAPE_SQUARE, SHAPE_TRIANGLE, SHAPE_CIRCLE, SHAPE_DIAMOND};
 
     @Override
     public void accept(List<KnnEvaluation> evaluations) {
 
-        plotTimesPerStrategyAndInstances(evaluations);
+        plotTimesPerStrategyAndDataset(evaluations);
     }
 
-    public void plotTimesPerStrategyAndInstances(List<KnnEvaluation> evaluations) {
+    public void plotTimesPerStrategyAndDataset(List<KnnEvaluation> evaluations) {
 
         try {
 
+            // Set general chart descriptions
+            String xAxisTitle = "Datasets";
+            String yAxisTitle = "Build + classification time in ms";
+            String title = "Total time per strategy and dataset";
+
+            // Retrieve number of strategies and datasets
+            int i = 0;
+            String prev = "";
             for (KnnEvaluation evaluation : evaluations) {
 
-                evaluation.getResult();
+                Object[] results = evaluation.getResult();
+                String actual = (String) results[0];
+                if ((i > 0) && !(actual.equals(prev))) break;
+                prev = actual;
+                i++;
+            }
+            int numStrategies = i;
+            int numDatasets = evaluations.size() / numStrategies;
+
+            // Set size of arrays
+            String[] legendLabels = new String[numStrategies];
+            Paint[] paints = TestDataGenerator.getRandomPaints(numStrategies);
+            Stroke[] strokes = new Stroke[numStrategies];
+            Shape[] shapes = new Shape[numStrategies];
+            String[] xAxisLabels = new String[numDatasets];
+            double[][] data = new double[numStrategies][numDatasets];
+
+            // Fill arrays from evaluation results
+            i = 0;
+            for (KnnEvaluation evaluation : evaluations) {
+
+                int datasetPos = i / numStrategies;
+                int strategyPos = i % numStrategies;
+
+                Object[] results = evaluation.getResult();
+
+                if (datasetPos == 0) {
+                    legendLabels[strategyPos] = (String) results[4];
+                    strokes[strategyPos] = DEFAULT_STROKE;
+                    shapes[strategyPos] = STANDARD_SHAPES[strategyPos % 4];
+                }
+
+                if (strategyPos == 0) {
+                    xAxisLabels[datasetPos] = (String) results[0];
+                }
+
+                data[strategyPos][datasetPos] = (Double) results[6] + (Double) results[7];
+
+                i++;
             }
 
-            String[] xAxisLabels = {"1998", "1999", "2000", "2001", "2002", "2003", "2004"};
-            String xAxisTitle = "Years";
-            String yAxisTitle = "Problems";
-            String title = "Micro$oft at Work";
-            DataSeries dataSeries = new DataSeries(xAxisLabels,
-                    xAxisTitle,
-                    yAxisTitle,
-                    title);
+//            String[] xAxisLabels = {"1998", "1999", "2000", "2001", "2002", "2003", "2004"};
+//
+//            double[][] data = new double[][]{{250, 45, -36, 66, 145, 80, 55},
+//                    {45, -36, 66, 145, 80, 55, 120},
+//                    {-36, 66, 145, 80, 55, 110, 140}};
+//            String[] legendLabels = {"Bugs", "Security Holes", "Backdoors"};
+//            Paint[] paints = TestDataGenerator.getRandomPaints(3);
+//            Stroke[] strokes = new Stroke[]{DEFAULT_STROKE, DEFAULT_STROKE, DEFAULT_STROKE};
+//            Shape[] shapes = new Shape[]{new Rectangle(15, 15), new Rectangle(8, 8), new Rectangle(7, 7)};
 
-            double[][] data = new double[][]{{250, 45, -36, 66, 145, 80, 55},
-                    {250, 45, -36, 66, 145, 80, 55},
-                    {250, 45, -36, 66, 145, 80, 55}};
-            String[] legendLabels = {"Bugs", "Security Holes", "Backdoors"};
-            Paint[] paints = TestDataGenerator.getRandomPaints(3);
-            Stroke[] strokes = new Stroke[]{DEFAULT_STROKE, DEFAULT_STROKE, DEFAULT_STROKE};
-            Shape[] shapes = new Shape[]{new Rectangle(15, 15), new Rectangle(8, 8), new Rectangle(7, 7)};
+            DataSeries dataSeries = new DataSeries(xAxisLabels,
+                                                    xAxisTitle,
+                                                    yAxisTitle,
+                                                    title);
+
             LineChartProperties lineChartProperties = new LineChartProperties(strokes, shapes);
             AxisChartDataSet axisChartDataSet = new AxisChartDataSet(data,
-                    legendLabels,
-                    paints,
-                    ChartType.LINE,
-                    lineChartProperties);
+                                                                    legendLabels,
+                                                                    paints,
+                                                                    ChartType.LINE,
+                                                                    lineChartProperties);
             dataSeries.addIAxisPlotDataSet(axisChartDataSet);
 
             ChartProperties chartProperties = new ChartProperties();
@@ -69,10 +122,11 @@ public class KnnBreakEvenCharts implements ResultProducer, Consumer<List<KnnEval
                     chartProperties,
                     axisProperties,
                     legendProperties,
-                    1980,
-                    1020);
+                    1200,
+                    900);
 
-            PNGEncoder.encode(axisChart, new FileOutputStream("charts/line-chart.png"));
+            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME).replace(":", "_");
+            PNGEncoder.encode(axisChart, new FileOutputStream(String.format("charts/%s_BreakEvenDatasets.png", timestamp)));
 
         } catch (Exception e) {
             throw new RuntimeException(e);
